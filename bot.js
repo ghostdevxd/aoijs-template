7e355ca6644bc1f4f2eff0de0e749d8b6aaa053b
ghostdev <87695542+ghostdevxd@users.noreply.github.com>
2022-06-05 11:40:19 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,36 @@
+const aoijs = require("aoi.js");
+ 
+const bot = new aoijs.Bot({
+  token: "TOKEN",  // .env dosyasına tokeni koyduysanız bu kısmı [ token: process.env.token, ] olarak değiştirin.
+  prefix: "!", // Botunuzun prefixini(ön ekini) belirleyin.
+  intents: "all", 
+  mobilePlatform: false, // Botunuz mobileden giriş yapıyormuş gibi bir simge çıkmasını istiyorsanız [ true ] yazın.
+  sharding: false, // Shard bağlamak istiyorsanız buraya [ true ] yazın.
+  shardAmount: 2, 
+})
+
+const loader = new aoijs.LoadCommands(bot)
+ loader.load(bot.cmd,"./komutlar/") // komutlar dosyasını tanımlayın
+
+bot.onMessage()
+
+bot.variables({
+ cat:"5", // botunuzun variablesleri
+})
+
+bot.readyCommand({
+    channel: "",
+    code: `
+$log[
+$userTag[$clientid] Giriş Yapıldı!
+$pingms - ping
+$serverCount - sunucu
+$allMembersCount - kullanıcı]`
+})
+
+bot.command({
+  name: "ping",
+  code: `
+  Ping $pingms
+  `,
+})
